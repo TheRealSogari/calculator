71a3f4c8fd7f30da9fd007028e5110ef75c90177
Sogari <therealsogari@gmail.com>
2022-11-26 05:43:30 +0100

    Add basic func and button functionality

---
+++ js/javascript.js
@@ -0,0 +1,26 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function mul(num1, num2) {
+    return num1 * num2;
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
+
+const allButtons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+
+allButtons.forEach(button => button.addEventListener("click", () => {
+    display.textContent = button.id;
+}));
